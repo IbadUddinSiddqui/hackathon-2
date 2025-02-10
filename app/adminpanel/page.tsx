fc25b510cdff9513f4ea919a9e861d5b161731a6
Ibad Uddin <ibaduddinsiddiqui418@gmail.com>
2025-02-10 20:09:04 +0500

    auth check again

--- app/adminpanel/page.tsx
+++ app/adminpanel/page.tsx
@@ -2,19 +2,24 @@ import ECommerce from "../components/Dashboard/E-commerce";
 import DefaultLayout from "../components/Layouts/DefaultLayout";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-export default async function  Home() {
-const session = await auth();
-    if (session?.user.role === "user") redirect("/denied");
+import { getToken } from "next-auth/jwt";
+export default async function Home() {
+  // Get the session
+  const session = await auth();
 
-  // If no session or user is not admin, redirect to home page
 
-  
-    return (
-      <>
-        <DefaultLayout>
-          <ECommerce />
-        </DefaultLayout>
-      </>
-    );
-  
+console.log(session?.user,"Admin session user")
+  // If there's no session or the user is not an admin, redirect to the denied page
+  if (!session || session?.user.email !== "ibaduddinsiddiqui418@gmail.com") {
+    redirect("/denied");
+  }
+
+  // If the user is an admin, render the admin panel
+  return (
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
+  );
 }
