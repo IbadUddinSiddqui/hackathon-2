a10282e7683879f257c6ac5fd1b3b34f410e86ed
Ibad Uddin <ibaduddinsiddiqui418@gmail.com>
2025-02-10 13:59:44 +0500

    auth check

--- app/adminpanel/page.tsx
+++ app/adminpanel/page.tsx
@@ -1,8 +1,10 @@
 import ECommerce from "../components/Dashboard/E-commerce";
 import DefaultLayout from "../components/Layouts/DefaultLayout";
-
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 export default async function  Home() {
-
+const session = await auth();
+    if (session?.user.role === "user") redirect("/denied");
 
   // If no session or user is not admin, redirect to home page
 
